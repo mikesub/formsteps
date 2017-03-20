877f09c2994e5c975116316d28b7ff5ba1abd1f4
Mikhail Saburenkov <allo.superman@gmail.com>
2017-03-20 21:33:58 +0300

    Style Submit Button

--- src/modules/SubmitButton/component.jsx
+++ src/modules/SubmitButton/component.jsx
@@ -1,11 +1,29 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import submit from './actions';
+import css from './index.css';
+
+const Message = ({ text }) => (text ? <div className={css.message}>{text}</div> : null);
+Message.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+const buttonText = (pending, error) => {
+  if (pending) {
+    return 'Submitting...';
+  }
+  if (error) {
+    return 'Try Again';
+  }
+  return 'Submit';
+};
 
 const SubmitButton = ({ pending, error, errorMessage, onClick }) => (
-  <div>
-    <button onClick={onClick} disabled={pending}>Submit{pending ? 'ting' : ''}</button>
-    {error ? errorMessage : null}
+  <div className={css.wrap}>
+    <button onClick={onClick} disabled={pending} className={css.button}>
+      {buttonText(pending, error)}
+    </button>
+    <Message text={error ? errorMessage : ''} />
   </div>
 );
 
